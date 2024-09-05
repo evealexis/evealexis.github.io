7d832899bc197bbe78fb9f8efe5b361f1fda0294
evealexis <alicia.e.alexis@gmail.com>
2024-09-05 21:41:29 +0100

    Updated index

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById("move-it").onclick = function () {
+    location.href = "https://evealexis.github.io/move-it/"
+};
+
+document.getElementById("move-it-github").onclick = function () {
+    location.href = "https://github.com/evealexis/move-it"
+};
+
+document.getElementById("simon-game").onclick = function () {
+    location.href = "https://evealexis.github.io/simon-game/"
+};
+
+document.getElementById("simon-game-github").onclick = function () {
+    location.href = "https://github.com/evealexis/simon-game"
+};
+
+document.getElementById("to-do").onclick = function () {
+    location.href = "https://evealexis.github.io/to-do/"
+};
+
+document.getElementById("to-do-github").onclick = function () {
+    location.href = "https://github.com/evealexis/to-do"
+};
